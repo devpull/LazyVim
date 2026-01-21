dc87f93f8bd7a57a1e01e482efbaff0afd1da173
devpull <devpull@yandex.ru>
2026-01-21 10:44:27 +0300

    chore: set html to be formatted by prettier

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,6 +4,7 @@ return {
     formatters_by_ft = {
       css = { "stylelint" },
       scss = { "stylelint" },
+      html = { "prettier" },
       -- Add other file types as needed
     },
     -- Other conform options
